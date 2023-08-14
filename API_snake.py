c62f4a0009719a928a20c5a1219be633e6d16495
Profs12345 <charlesmontangerand@gmail.com>
2023-08-14 20:56:49 +0200

    Create API_snake.py

---
+++ API_snake.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        return "Fichier introuvable"
+    except Exception as e:
+        return str(e)
+
+def write_to_file(file_path, new_content):
+    try:
+        with open(file_path, 'w') as file:
+            file.write(new_content)
+        return "Contenu du fichier mis à jour"
+    except Exception as e:
+        return str(e)
+
+@app.route('/api/get_content', methods=['GET'])
+def get_content():
+    return jsonify(content=read_file("highscore.txt"))
+
+@app.route('/api/update_content', methods=['POST'])
+def update_content():
+    data = request.get_json()
+    new_content = data.get('new_content')
+    result = write_to_file("highscore.txt", new_content)
+    return jsonify(message=result)
+
+
+app.run()
